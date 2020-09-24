29a760df8561278ae2fa612b3a9c989163d6ef73
Bharat Patodi <bharatpatodi@gmail.com>
2020-09-24 20:00:11 +0530

    Selecting and replacing

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let bodyText = document.body.innerText
+
+replacer = (element) => {
+    if(element.hasChildNodes()) {
+        element.childNodes.forEach(replacer);
+    } else if(element.nodeType === Text.TEXT_NODE) {
+        console.log(element.textContent);
+        element.textContent = element.textContent.replace(
+          /government/gi,
+          "<span>सरकार</span>"
+        );
+    }
+}
+
+replacer(document.body);
