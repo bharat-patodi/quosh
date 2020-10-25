c21a4a2a32beb3f46378e3cdb58b366e0766f7d7
Bharat Patodi <bharatpatodi@gmail.com>
2020-10-25 13:44:26 +0530

    Refactor Modal Code

--- modal.js
+++ modal.js
@@ -1,39 +1,36 @@
-let closeButton = document.querySelector('.close-button');
+let closeButton = document.querySelector('.quosh__modal__close__button');
 
 function handleClose(){
 	window.close();
 }
 closeButton.addEventListener('click', handleClose);
 
-
-
-
 // New
 
-function toggle () 
+function toggle ()
 {
-	var checkBox = document.querySelector("#checkbox");
+	var checkBox = document.querySelector("#quosh__modal__toggle__checkbox");
 	var active = document.getElementById("active");
 	var inactive = document.getElementById("inactive");
-	
+
 	var check = checkBox.checked;
-		
-	chrome.storage.local.set({'toggle': check}, function() 
+
+	chrome.storage.local.set({'toggle': check}, function()
 	{
-        if (chrome.extension.lastError) 
+        if (chrome.extension.lastError)
 		{
             alert('An error occurred: ' + chrome.extension.lastError.message);
         }
     });
-		
+
 	if(check == true)
 	{
 		inactive.style.display = "none";
 		active.style.display = "block";
 
 		chrome.tabs.query({}, tabs => {
-			tabs.forEach(tab => 
-			  chrome.tabs.sendMessage(tab.id, {greeting: "active"}), function(response) 
+			tabs.forEach(tab =>
+			  chrome.tabs.sendMessage(tab.id, {greeting: "active"}), function(response)
 			  	{
 			  		console.log(response.farewell);
 			  	}
@@ -45,8 +42,8 @@ function toggle ()
 		inactive.style.display = "block";
 		active.style.display = "none";
 		chrome.tabs.query({}, tabs => {
-			tabs.forEach(tab => 
-			  chrome.tabs.sendMessage(tab.id, {greeting: "inactive"}), function(response) 
+			tabs.forEach(tab =>
+			  chrome.tabs.sendMessage(tab.id, {greeting: "inactive"}), function(response)
 			  	{
 			  		console.log(response.farewell);
 			  	}
@@ -57,12 +54,12 @@ function toggle ()
 
 function afterLoaded()
 {
-	document.querySelector('#checkbox').addEventListener("click", toggle);
-	
-	var checkBox = document.querySelector("#checkbox");
+	var checkBox = document.querySelector("#quosh__modal__toggle__checkbox");
+	checkBox.addEventListener("click", toggle);
+
 	var active = document.getElementById("active");
 	var inactive = document.getElementById("inactive");
-	
+
 	chrome.storage.local.get('toggle', function(result)
 	{
 		var check = result.toggle;
@@ -83,11 +80,11 @@ function afterLoaded()
 	});
 }
 
-if(document.readyState === 'loading') 
+if(document.readyState === 'loading')
 {
     document.addEventListener('DOMContentLoaded', afterLoaded);
-} 
-else 
+}
+else
 {
     afterLoaded();
 }
